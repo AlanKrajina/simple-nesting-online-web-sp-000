97a895770a2c38eceff6ea65c65c5b179c423750
Alan Krajina <alankrajina88@gmail.com>
2019-05-14 16:39:41 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -82,7 +82,7 @@ def adding_matz
           :languages => ["C"]
         }
      }
-programmer_hash[:yukihiro_matsumoto].unshift([:known_for => "Ruby",:language => ["LISP", "C"]])
+programmer_hash[:yukihiro_matsumoto].unshift([:known_for => "Ruby",:language => ["LISP", "C"])
     
 end
 
