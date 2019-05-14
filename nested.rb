1d9c26cce8031982b198e951f0e2c374b13d41ce
Alan Krajina <alankrajina88@gmail.com>
2019-05-14 16:44:55 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -62,10 +62,12 @@ end
 
 def adding_matz
  
-  :yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#    :languages => ["LISP", "C"]
-# }
+  hash = }
+    :yukihiro_matsumoto => {
+    :known_for => "Ruby",
+    :languages => ["LISP", "C"] 
+    
+  }}
 
 	programmer_hash = 
  		{
